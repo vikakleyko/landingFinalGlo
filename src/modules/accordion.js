42b1f04ccd5a608eeb4cf4545c4699620316bd06
Viktoriia Kleiko <vika.kleyko@gmail.com>
2020-07-22 21:15:57 +0300

    accordion,  collase answer the selected question and hide the rest of them when user click on it

--- src/modules/accordion.js
+++ src/modules/accordion.js
@@ -1,7 +1,21 @@
 "use strict";
 
 const accordion = () => {
+  const panel = document.getElementById("accordion-two");
+  console.log(panel);
 
+  panel.addEventListener("click", (e) => {
+    let target = e.target;
+
+    const blocks = panel.querySelectorAll(".panel-collapse");
+
+    const question = target.closest(".panel-default");
+    const answer = question.querySelector(".panel-collapse");
+    blocks.forEach((block) => {
+      block.classList.remove("in");
+    });
+    answer.classList.add("in");
+  });
 };
 
 export default accordion;
