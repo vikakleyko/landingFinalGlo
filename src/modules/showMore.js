749c9edbd591ddd80791667f1810376bd9e734b3
Viktoriia Kleiko <vika.kleyko@gmail.com>
2020-07-22 19:40:33 +0300

    additional blocks appear and button dissapears when user clicks on button with class add-sentence-btn

--- src/modules/showMore.js
+++ src/modules/showMore.js
@@ -1,7 +1,18 @@
 "use strict";
 
 const showMore = () => {
+  const btn = document.querySelector(".add-sentence-btn"),
+    additionalBlocks = document.querySelectorAll(".shadow-block ");
 
+  btn.addEventListener("click", () => {
+    btn.style.display = "none";
+
+    additionalBlocks.forEach((block) => {
+      block.style.visibility = "visible";
+      block.closest(".col-xs-12").classList.remove("hidden");
+      block.closest(".col-xs-12").classList.remove("visible-sm-block");
+    });
+  });
 };
 
 export default showMore;
