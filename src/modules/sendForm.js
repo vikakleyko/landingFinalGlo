275a43912f7a1a7a158d4dc370b6cdd22f3fa737
Viktoriia Kleiko <vika.kleyko@gmail.com>
2020-07-28 20:26:44 +0700

    phone mask added

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,5 +1,47 @@
 "use strict";
 
+
+const maskPhone = (selector, masked = '+7 (___) ___-__-__') => {
+	const elems = document.querySelectorAll(selector);
+
+	function mask(event) {
+		const keyCode = event.keyCode;
+		const template = masked,
+			def = template.replace(/\D/g, ""),
+			val = event.target.value.replace(/\D/g, "");
+		let i = 0,
+			newValue = template.replace(/[_\d]/g, function (a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+			});
+		i = newValue.indexOf("_");
+		if (i !== -1) {
+			newValue = newValue.slice(0, i);
+		}
+		let reg = template.substr(0, event.target.value.length).replace(/_+/g,
+			function (a) {
+				return "\\d{1," + a.length + "}";
+			}).replace(/[+()]/g, "\\$&");
+		reg = new RegExp("^" + reg + "$");
+		if (!reg.test(event.target.value) || event.target.value.length < 5 || keyCode > 47 && keyCode < 58) {
+			event.target.value = newValue;
+		}
+		if (event.type === "blur" && event.target.value.length < 5) {
+			event.target.value = "";
+		}
+
+	}
+
+	for (const elem of elems) {
+		elem.addEventListener("input", mask);
+		elem.addEventListener("focus", mask);
+		elem.addEventListener("blur", mask);
+	}
+};
+
+// use
+
+maskPhone('.phone-user', '+7 (___) ___-__-__');
+
 const sendForm = () => {
   const forms = document.querySelectorAll("form"),
     question = document.querySelector(".user_quest"),
@@ -93,9 +135,9 @@ const sendForm = () => {
   };
 
   const send = () => {
-    const valideNumber = (number) => {
-      const phonePattern = /^[+]?\d{10}/;
-      return !!(number && number.match(phonePattern));
+    const valideNumber = (input) => {
+      const number = input.replace(/[^0-9]/g, '');
+      return !!(input && number.length === 11);
     };
 
     const valideText = (text) => {
@@ -116,7 +158,7 @@ const sendForm = () => {
     const timerToClearMessage = () =>
       setTimeout(() => {
         statusMessage.textContent = "";
-      }, 7000);
+      }, 4000);
 
     forms.forEach((form) => {
       form.addEventListener("submit", (e) => {
@@ -144,11 +186,10 @@ const sendForm = () => {
         } else {
           data = { body, order };
         }
-
-        if (inputName && inputPhone) {
-          valide = valideNumber(inputPhone) && valideText(inputName);
-        } else {
+        if (typeof inputName === "undefined") {
           valide = valideNumber(inputPhone);
+        } else {
+          valide = valideNumber(inputPhone) && valideText(inputName);
         }
 
         if (valide) {
@@ -166,6 +207,7 @@ const sendForm = () => {
               clearInputs(form);
             });
         } else {
+          timerToClearMessage();
           statusMessage.textContent = invalidInputaMessage;
         }
       });
